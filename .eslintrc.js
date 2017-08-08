a13a81369b283f12e31e277c449107bc74d7e01c
Mario Fernandez <mario.fernandez@xing.com>
2017-08-08 22:02:08 +0200

    add eslint and fix warnings

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    mocha: true,
+    "jest/globals": true
+  },
+  "extends": [
+    "standard",
+    "standard-react",
+    "plugin:flowtype/recommended"
+  ],
+  "plugins": [
+    "jest",
+    "flowtype"
+  ],
+  "globals": {
+    'URL': true,
+    'fetch': true
+  }
+}
