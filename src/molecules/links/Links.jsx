be646a0227d14a824d2b68890ffb8ed7dd1b739e
Mario Fernandez <mario.fernandez@xing.com>
2017-08-20 13:59:23 +0200

    Simplify code

--- src/molecules/links/Links.jsx
+++ src/molecules/links/Links.jsx
@@ -3,25 +3,23 @@ import React from 'react'
 import { Card, CardTitle, CardBlock, ListGroup, ListGroupItem } from 'reactstrap'
 
 const Links = () => (
-  <div>
-    <Card>
-      <CardBlock>
-        <CardTitle tag='h5' className='mb-0'>Links</CardTitle>
-      </CardBlock>
-      <ListGroup flush>
-        <ListGroupItem>
-          <a href='https://github.com/sirech'>
-            Github Repositories
-          </a>
-        </ListGroupItem>
-        <ListGroupItem>
-          <a href='https://stackoverflow.com/users/3785/mario-f'>
-            Stackoverflow
-          </a>
-        </ListGroupItem>
-      </ListGroup>
-    </Card>
-  </div>
+  <Card>
+    <CardBlock>
+      <CardTitle tag='h5' className='mb-0'>Links</CardTitle>
+    </CardBlock>
+    <ListGroup flush>
+      <ListGroupItem>
+        <a href='https://github.com/sirech'>
+          Github Repositories
+        </a>
+      </ListGroupItem>
+      <ListGroupItem>
+        <a href='https://stackoverflow.com/users/3785/mario-f'>
+          Stackoverflow
+        </a>
+      </ListGroupItem>
+    </ListGroup>
+  </Card>
 )
 
 export default Links
--- src/molecules/talks/Talks.jsx
+++ src/molecules/talks/Talks.jsx
@@ -9,22 +9,26 @@ const talks = [
   ['https://github.com/sirech/talks/blob/master/2017-04-rubygroupmunich-yai.pdf', 'Assign PRs automatically with YAI']
 ]
 
+const renderTalk = (url, title) => (
+  <ListGroupItem key={url}>
+    <a href={url}>{title}</a>
+  </ListGroupItem>
+)
+
+const renderTalks = () => (
+  talks.map(([url, title]) => renderTalk(url, title))
+)
+
 const Talks = () => (
-  <div>
-    <Card>
-      <CardBlock>
-        <CardTitle tag='h5' className='mb-0'>Talks</CardTitle>
-        <CardText>I used to hate talking in front of people. But I have done technical presentations for a while, and at some point I started to like it. Not that I have mastered, though. For instance, I still tend to talk too fast. I have mostly done technical presentations, at work and at ruby meetups. Hope to eventually get some experience with bigger crowds</CardText>
-      </CardBlock>
-      <ListGroup flush>
-        {talks.map(([url, title]) =>
-          <ListGroupItem key={url}>
-            <a href={url}>{title}</a>
-          </ListGroupItem>
-        )}
-      </ListGroup>
-    </Card>
-  </div>
+  <Card>
+    <CardBlock>
+      <CardTitle tag='h5' className='mb-0'>Talks</CardTitle>
+      <CardText>I used to hate talking in front of people. But I have done technical presentations for a while, and at some point I started to like it. Not that I have mastered, though. For instance, I still tend to talk too fast. I have mostly done technical presentations, at work and at ruby meetups. Hope to eventually get some experience with bigger crowds</CardText>
+    </CardBlock>
+    <ListGroup flush>
+      {renderTalks()}
+    </ListGroup>
+  </Card>
 )
 
 export default Talks
