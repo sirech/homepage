3224b27e4980506a9c6a6e5b2a68148ddf8b4cfa
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-01-31 22:21:22 +0100

    Add ruby on ice talk

--- src/molecules/talks/Talks.jsx
+++ src/molecules/talks/Talks.jsx
@@ -7,8 +7,9 @@ const talks = [
   ['https://github.com/sirech/talks/raw/master/2016-04-rubygroupmunich-rubocop.pdf', 'Using RuboCop'],
   ['https://github.com/sirech/talks/raw/master/2017-03-es2017.pdf', 'Intro to ES2017'],
   ['https://github.com/sirech/talks/blob/master/2017-04-rubygroupmunich-yai.pdf', 'Assign PRs automatically with YAI'],
-  ['https://github.com/sirech/talks/raw/master/2017-11-tw-onboarding.pdf', 'Welcoming new developers into a team']
-]
+  ['https://github.com/sirech/talks/raw/master/2017-11-tw-onboarding.pdf', 'Welcoming new developers into a team'],
+  ['https://github.com/sirech/talks/raw/master/2018-01-rubyonice-maintainingrailsapps.pdf', 'Ruby on Ice 2018 - Maintaining Rails Applications through the years']
+].reverse()
 
 const renderTalk = (url, title) => (
   <ListGroupItem key={url}>
@@ -17,14 +18,14 @@ const renderTalk = (url, title) => (
 )
 
 const renderTalks = () => (
-  talks.reverse().map(([url, title]) => renderTalk(url, title))
+  talks.map(([url, title]) => renderTalk(url, title))
 )
 
 const Talks = () => (
   <Card className='mb-3'>
     <CardHeader tag='h5'>Talks</CardHeader>
     <CardBody>
-      <CardText>I used to hate talking in front of people. But I have done technical presentations for a while, and at some point I started to like it. Not that I have mastered, though. For instance, I still tend to talk too fast. I have mostly done technical presentations, at work and at ruby meetups. Hope to eventually get some experience with bigger crowds</CardText>
+      <CardText>I used to hate talking in front of people, but after doing presentations for a while, at some point I started to like it. Still have plenty to learn though.</CardText>
     </CardBody>
     <ListGroup flush>
       {renderTalks()}
