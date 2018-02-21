7fdfcb671629bd6c8a5de254f90cd4f43e099890
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-02-21 12:54:06 +0100

    Update talks

--- src/molecules/talks/Talks.jsx
+++ src/molecules/talks/Talks.jsx
@@ -8,7 +8,8 @@ const talks = [
   ['https://github.com/sirech/talks/raw/master/2017-03-es2017.pdf', 'Intro to ES2017'],
   ['https://github.com/sirech/talks/blob/master/2017-04-rubygroupmunich-yai.pdf', 'Assign PRs automatically with YAI'],
   ['https://github.com/sirech/talks/raw/master/2017-11-tw-onboarding.pdf', 'Welcoming new developers into a team'],
-  ['https://github.com/sirech/talks/raw/master/2018-01-rubyonice-maintainingrailsapps.pdf', 'Ruby on Ice 2018 - Maintaining Rails Applications through the years']
+  ['https://github.com/sirech/talks/raw/master/2018-01-rubyonice-maintainingrailsapps.pdf', 'Ruby on Ice 2018 - Maintaining Rails Applications through the years'],
+  ['https://github.com/sirech/talks/raw/master/2018-02-tdd_infrastructure.pdf', 'TDD for Infrastructure']
 ].reverse()
 
 const renderTalk = (url, title) => (
